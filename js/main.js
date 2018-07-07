b25931bebcee295a611c3099677c1116b1c943d8
Cassio Conti <cassiorconti@gmail.com>
2018-07-06 21:28:59 -0500

    fix: small UI elements change to be more semantically right

--- js/main.js
+++ js/main.js
@@ -169,7 +169,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = `Image representing the restaurant ${restaurant.name}`;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
