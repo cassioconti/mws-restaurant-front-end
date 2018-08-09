a39fc36ae8a05c3130e5b5328526d2221894bc78
Cassio Conti <cassiorconti@gmail.com>
2018-08-09 15:55:08 -0500

    Caching the reviews separately from the restaurants

--- js/idbhelper.js
+++ js/idbhelper.js
@@ -3,15 +3,22 @@ const idb = require('idb');
 /** Handles the IDB */
 class IdbHelper {
     constructor() {
-        this._dbPromise = idb.open('restaurant', 1, function (upgradeDb) {
+        this._dbPromiseRestaurant = idb.open('restaurant', 1, function (upgradeDb) {
             var store = upgradeDb.createObjectStore('restaurants', {
                 keyPath: 'id'
             });
         });
+
+        this._dbPromiseReview = idb.open('review', 1, function (upgradeDb) {
+            var store = upgradeDb.createObjectStore('reviews', {
+                keyPath: 'id'
+            });
+            store.createIndex('restaurant_id', 'restaurant_id');
+        });
     }
 
     addRestaurants(restaurants) {
-        return this._dbPromise.then(function (db) {
+        return this._dbPromiseRestaurant.then(function (db) {
             if (!db) return;
 
             var tx = db.transaction('restaurants', 'readwrite');
@@ -23,16 +30,37 @@ class IdbHelper {
     }
 
     getAllRestaurants() {
-        return this._dbPromise.then(db => {
+        return this._dbPromiseRestaurant.then(db => {
             return db.transaction('restaurants')
                 .objectStore('restaurants').getAll();
         });
     }
 
     getRestaurant(id) {
-        return this._dbPromise.then(db => {
+        return this._dbPromiseRestaurant.then(db => {
             return db.transaction('restaurants')
                 .objectStore('restaurants').get(id);
+        });
+    }
+
+    addReviews(reviews) {
+        return this._dbPromiseReview.then(function (db) {
+            if (!db) return;
+
+            var tx = db.transaction('reviews', 'readwrite');
+            var store = tx.objectStore('reviews');
+            reviews.forEach(function (review) {
+                store.put(review);
+            });
+        });
+    }
+
+    getAllReviewsForRestaurant(restaurantId) {
+        return this._dbPromiseReview.then(db => {
+            return db.transaction('reviews')
+                .objectStore('reviews')
+                .index('restaurant_id')
+                .getAll(restaurantId);
         });
     }
 }
